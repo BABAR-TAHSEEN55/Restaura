33409a5dff62dc8ed26a9165ce1eca863e517452
Babar <babarsmoke24@gmail.com>
2025-01-25 23:23:57 +0530

    Social Links added

---
+++ src/components/Footex.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { SOCIAL_MEDIA_LINKS } from "../../restaura-assets/constants";
+
+const Footer = () => {
+  return (
+    <div className="mb-8 mt-20">
+      <div className="flex items-center justify-center gap-8 text-white">
+        {SOCIAL_MEDIA_LINKS.map((social, index) => (
+          <a
+            key={index}
+            href={social.href}
+            target="_bland"
+            rel=" noopener noreferrer"
+          >
+            {social.icon}
+          </a>
+        ))}
+      </div>
+      <p className="mt-8 text-center tracking-tighter text-neutral-500">
+        All rights Reserved
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
